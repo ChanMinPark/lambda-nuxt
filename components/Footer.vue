50aa03f5de2a5b00dfab7cd19a729bb06db2661f
ChanMinPark <walkinpcm@gmail.com>
2019-02-14 16:49:25 +0900

    Footer 스타일 수정

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer>
     <div class="container">
-      <dir class="text-center" style="margin: 50px 0;">
+      <dir class="text-center" style="margin: 50px 0;padding: 0;">
         - ChanMin Park (walkinpcm@gmail.com) -
       </dir>
     </div>
