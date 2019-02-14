5c1369c02e36f842f8f19d0cdf398a7a404dcd96
ChanMinPark <walkinpcm@gmail.com>
2019-02-14 16:52:16 +0900

    site renew

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,119 @@
 <template>
-  <section>
-    <section id="main-sec-1">
-      <div class="container">
-        <h1>What is Lorem Ipsum?</h1>
-        <h2>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</h2>
+  <section id="page-main">
+    <!-- Main Section -->
+    <section id="sec-1">
+      <div class="container" style="position: relative; height: 100%;">
+        <div class="content-box">
+          <img src="https://s3.ap-northeast-2.amazonaws.com/walkinpcm-web-public-resources/lambda-nuxt/images/lambda-nuxt-thumbnail.jpg" alt="Nuxt.js with AWS Serverless">
+        </div>
       </div>
     </section>
-    <section id="main-sec-2">
+
+    <!-- Nuxt.js Introduction -->
+    <section id="sec-2">
       <div class="container">
-        <h1>Where does it come from?</h1>
-        <h2>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC</h2>
+        <div class="content-box">
+          <ul class="list-unstyled row">
+            <li class="col-sm-5">
+              <div class="logo-box">
+                <img src="https://s3.ap-northeast-2.amazonaws.com/walkinpcm-web-public-resources/lambda-nuxt/images/nuxt-logo.png" alt="Nuxt.js">
+              </div>
+            </li>
+            <li class="col-sm-7">
+              <div class="desc-box">
+                <h1 class="section-title">What is Nuxt.js</h1>
+                <p class="section-desc">
+                  It's main scope is UI rendering while abstracting away the client/server distribution.<br>
+                  <br>
+                  Our goal is to create a framework flexible enough that you can use it as a main project base or in addition to your current project based on Node.js.<br>
+                  <br>
+                  Nuxt.js presets all the configuration needed to make your development of a Vue.js Application Server Rendered more enjoyable.
+                </p>
+                <br>
+                <br>
+                <a href="https://nuxtjs.org/" target="_blank">Nuxt.js website >></a>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </section>
-    <section id="main-sec-3">
+
+    <!-- AWS Serverless Introduction -->
+    <section id="sec-3">
       <div class="container">
-        <h1>Why do we use it?</h1>
-        <h2>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</h2>
+        <div class="content-box">
+          <ul class="list-unstyled row">
+            <li class="col-sm-5">
+              <div class="logo-box">
+                <img src="https://s3.ap-northeast-2.amazonaws.com/walkinpcm-web-public-resources/lambda-nuxt/images/aws-severless-logo.png" alt="AWS Serverless">
+              </div>
+            </li>
+            <li class="col-sm-7">
+              <div class="desc-box">
+                <h1 class="section-title">What is AWS Serverless</h1>
+                <p class="section-desc">
+                  Serverless is the native architecture of the cloud that enables you to shift more of your operational responsibilities to AWS, increasing your agility and innovation. Serverless allows you to build and run applications and services without thinking about servers. It eliminates infrastructure management tasks such as server or cluster provisioning, patching, operating system maintenance, and capacity provisioning. You can build them for nearly any type of application or backend service, and everything required to run and scale your application with high availability is handled for you.
+                </p>
+                <br>
+                <br>
+              </div>
+            </li>
+          </ul>
+
+          <div class="service-list">
+            <h2 class="list-title">Serverless Services</h2>
+            <ul class="list-unstyled row">
+              <li class="col-12 col-sm-6 col-md-3">
+                <div class="service-card">
+                  <div class="service-card__logo">
+                    <img src="https://s3.ap-northeast-2.amazonaws.com/walkinpcm-web-public-resources/lambda-nuxt/images/serverless-lambda.svg" alt="Lambda">
+                  </div>
+                  <div class="service-card__name">
+                    Lambda
+                  </div>
+                </div>
+              </li>
+              <li class="col-12 col-sm-6 col-md-3">
+                <div class="service-card">
+                  <div class="service-card__logo">
+                    <img src="https://s3.ap-northeast-2.amazonaws.com/walkinpcm-web-public-resources/lambda-nuxt/images/serverless-apigw.svg" alt="API Gateway">
+                  </div>
+                  <div class="service-card__name">
+                    API Gateway
+                  </div>
+                </div>
+              </li>
+              <li class="col-12 col-sm-6 col-md-3">
+                <div class="service-card">
+                  <div class="service-card__logo">
+                    <img src="https://s3.ap-northeast-2.amazonaws.com/walkinpcm-web-public-resources/lambda-nuxt/images/serverless-cf.svg" alt="CloudFront">
+                  </div>
+                  <div class="service-card__name">
+                    CloudFront
+                  </div>
+                </div>
+              </li>
+              <li class="col-12 col-sm-6 col-md-3">
+                <div class="service-card">
+                  <div class="service-card__logo">
+                    <img src="https://s3.ap-northeast-2.amazonaws.com/walkinpcm-web-public-resources/lambda-nuxt/images/serverless-route53.svg" alt="Route53">
+                  </div>
+                  <div class="service-card__name">
+                    Route53
+                  </div>
+                </div>
+              </li>
+            </ul>
+
+            <div class="service-detail-link">
+              <nuxt-link :to="{ name: 'serverless' }">+ View Detail +</nuxt-link>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
+
   </section>
 </template>
 
@@ -31,38 +127,217 @@ export default {
 }
 </script>
 
-<style scoped>
-h1, h2 {
-  color: #fff;
-  font-weight: 700;
-  text-shadow: 3px 3px 3px #333;
-}
+<style lang="scss" scoped>
+#page-main {
+  #sec-1 {
+    position: relative;
+    width: 100%;
+    height: 100vh;
+    background-position: center;
+    background-size: cover;
+    background-image: url('https://s3.ap-northeast-2.amazonaws.com/walkinpcm-web-public-resources/lambda-nuxt/images/houses-1622066_1920.jpg');
 
-h1 {
-  font-size: 70px;
-}
+    .content-box {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 100%;
+      background-color: #fff;
+      border-radius: 50px;
+      overflow: hidden;
 
-h2 {
-  margin-top: 30px;
-}
+      @media (max-width: 575.98px) {
+        width: 90%;
+      }
 
-#main-sec-1,
-#main-sec-2,
-#main-sec-3 {
-  width: 100%;
-  height: 100vh;
-  background-position: center;
-  background-size: cover;
-  padding-top: 200px;
-}
+      > img {
+        width: 100%;
+        height: auto;
 
-#main-sec-1 {
-  background-image: url('https://s3.ap-northeast-2.amazonaws.com/walkinpcm-web-public-resources/lambda-nuxt/images/field-3535119_1920.jpg'); 
-}
-#main-sec-2 {
-  background-image: url('https://s3.ap-northeast-2.amazonaws.com/walkinpcm-web-public-resources/lambda-nuxt/images/houses-1622066_1920.jpg'); 
-}
-#main-sec-3 {
-  background-image: url('https://s3.ap-northeast-2.amazonaws.com/walkinpcm-web-public-resources/lambda-nuxt/images/fruit-3661159_1920.jpg'); 
+      }
+    }
+  }
+
+  #sec-2 {
+    padding: 150px 0;
+
+    .content-box {
+      > ul {
+        margin-left: -30px;
+        margin-right: -30px;
+
+        > li {
+          padding-left: 30px;
+          padding-right: 30px;
+
+          .logo-box {
+            position: relative;
+            padding: 30px;
+            box-shadow: 3px 3px 10px #999;
+            border-radius: 50px;
+            background-color: #fff;
+
+            > img {
+              width: 100%;
+            }
+          }
+
+          .desc-box {
+            p {
+              margin-top: 30px;
+            }
+          }
+        }
+      }
+    }
+
+    @media (max-width: 575.98px) {
+      padding: 50px 20px;
+
+      .content-box {
+        > ul {
+          > li {
+            .logo-box {
+              width: 250px;
+              border-radius: 30px;
+            }
+
+            .desc-box {
+              margin-top: 30px;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  #sec-3 {
+    padding: 150px 0;
+    background-color: #232f3e;
+    color: #fff;
+
+    .content-box {
+      > ul {
+        margin-left: -30px;
+        margin-right: -30px;
+
+        > li {
+          padding-left: 30px;
+          padding-right: 30px;
+
+          .logo-box {
+            position: relative;
+            padding: 30px;
+            box-shadow: 3px 3px 10px #999;
+            border-radius: 50px;
+            background-color: #fff;
+            overflow: hidden;
+
+            > img {
+              width: 100%;
+            }
+          }
+
+          .desc-box {
+            h1, p {
+              color: #fff;
+            }
+
+            p {
+              margin-top: 30px;
+            }
+          }
+        }
+      }
+
+      .service-list {
+        margin-top: 80px;
+
+        .list-title {
+          text-align: center;
+        }
+
+        > ul {
+          margin-top: 50px;
+
+          > li {
+            margin-top: 30px;
+
+            .service-card {
+              .service-card {
+                &__logo {
+                  position: relative;
+                  border-radius: 15px;
+                  overflow: hidden;
+
+                  > img {
+                    width: 100%;
+                    transform: scale(1.0);
+                    transition: transform .3s ease;
+                  }
+
+                  &:hover {
+                    > img {
+                      transform: scale(1.1);
+                    }
+                  }
+                }
+
+                &__name {
+                  margin-top: 10px;
+                  text-align: center;
+                  font-size: 26px;
+                }
+              }
+            }
+          }
+        }
+
+        .service-detail-link {
+          text-align: center;
+          margin-top: 40px;
+
+          > a {
+            font-size: 24px;
+          }
+        }
+      }
+    }
+
+    @media (max-width: 575.98px) {
+      padding: 50px 20px;
+
+      .content-box {
+        > ul {
+          > li {
+            .logo-box {
+              width: 250px;
+              border-radius: 30px;
+            }
+
+            .desc-box {
+              margin-top: 30px;
+            }
+          }
+        }
+
+        .service-list {
+          > ul {
+            > li {
+              .service-card {
+                .service-card {
+                  &__logo {
+                    width: 200px;
+                    margin: 0 auto;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 }
 </style>
---
+++ pages/serverless/index.vue
@@ -0,0 +1,118 @@
+<template>
+  <div id="page-serverless">
+    <section class="service-section" v-for="(item, index) in serviceList" :key="index" :class="{ 'back-dark': index % 2 === 0 }">
+      <div class="container">
+        <ul class="list-unstyled row">
+          <li class="col-sm-2">
+            <div class="logo-box">
+              <img :src="item.thumbnail" :alt="item.name">
+            </div>
+          </li>
+          <li class="col-sm-10">
+            <div class="desc-box">
+              <h1 class="section-title">{{ item.name }}</h1>
+              <p class="section-desc">
+                {{ item.description }}
+              </p>
+              <br>
+              <br>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+
+    <div class="back-home">
+      <nuxt-link :to="{ name: 'index' }">Back to HOME</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        serviceList: [
+          {
+            name: 'Lambda',
+            description: `AWS Lambda is a serverless compute service that runs your code in response to events and automatically manages the underlying compute resources for you. You can use AWS Lambda to extend other AWS services with custom logic, or create your own back-end services that operate at AWS scale, performance, and security. AWS Lambda can automatically run code in response to multiple events, such as HTTP requests via Amazon API Gateway, modifications to objects in Amazon S3 buckets, table updates in Amazon DynamoDB, and state transitions in AWS Step Functions.`,
+            thumbnail: 'https://s3.ap-northeast-2.amazonaws.com/walkinpcm-web-public-resources/lambda-nuxt/images/serverless-lambda.svg'
+          },
+          {
+            name: 'API Gateway',
+            description: `Amazon API Gateway is a fully managed service that makes it easy for developers to publish, maintain, monitor, secure, and operate APIs at any scale. It's a pay-as-you-go service that takes care of all of the undifferentiated heavy lifting involved in securely and reliably running APIs at scale.`,
+            thumbnail: 'https://s3.ap-northeast-2.amazonaws.com/walkinpcm-web-public-resources/lambda-nuxt/images/serverless-apigw.svg'
+          },
+          {
+            name: 'CloudFront',
+            description: `Amazon CloudFront is a fast content delivery network (CDN) service that securely delivers data, videos, applications, and APIs to customers globally with low latency, high transfer speeds, all within a developer-friendly environment. CloudFront is integrated with AWS – both physical locations that are directly connected to the AWS global infrastructure, as well as other AWS services. CloudFront works seamlessly with services including AWS Shield for DDoS mitigation, Amazon S3, Elastic Load Balancing or Amazon EC2 as origins for your applications, and Lambda@Edge to run custom code closer to customers’ users and to customize the user experience.`,
+            thumbnail: 'https://s3.ap-northeast-2.amazonaws.com/walkinpcm-web-public-resources/lambda-nuxt/images/serverless-cf.svg'
+          },
+          {
+            name: 'Route53',
+            description: `Amazon Route 53 is a highly available and scalable cloud Domain Name System (DNS) web service. It is designed to give developers and businesses an extremely reliable and cost effective way to route end users to Internet applications by translating names like www.example.com into the numeric IP addresses like 192.0.2.1 that computers use to connect to each other. Amazon Route 53 is fully compliant with IPv6 as well.`,
+            thumbnail: 'https://s3.ap-northeast-2.amazonaws.com/walkinpcm-web-public-resources/lambda-nuxt/images/serverless-route53.svg'
+          }
+        ]
+      }
+    } 
+  }
+</script>
+
+<style lang="scss" scoped>
+#page-serverless {
+  .service-section {
+    padding: 100px 0;
+    background-color: #fff;
+    color: #000;
+
+    h1 {
+      color: #000;
+    }
+
+    &.back-dark {
+      background-color: #232f3e;
+      color: #fff;
+
+      h1 {
+        color: #fff;
+      }
+    }
+
+    ul {
+      > li {
+        .desc-box {
+          > p {
+            margin-top: 30px;
+          }
+        }
+      }
+    }
+  }
+
+  .back-home {
+    text-align: center;
+
+    > a {
+      font-size: 28px;
+    }
+  }
+
+  @media (max-width: 575.98px) {
+    .service-section {
+      padding: 80px 20px;
+
+      ul {
+        > li {
+          .desc-box {
+            > h1 {
+              margin-top: 15px;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
+
