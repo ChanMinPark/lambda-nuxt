01278b98ef87bec6813a5179deab28441ad5cbc4
ChanMinPark <walkinpcm@gmail.com>
2019-02-14 15:24:57 +0900

    footer 내용 수정

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <footer>
     <div class="container">
       <dir class="text-center" style="margin: 50px 0;">
-        Visit our website for more documentation : <a href="https://nuxtjs.org" target="_blank">nuxtjs.org</a>
+        - ChanMin Park (walkinpcm@gmail.com) -
       </dir>
     </div>
   </footer>
